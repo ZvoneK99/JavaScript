7f4c576e2983f12e7136e7986dd1c350972c79f6
Zvone <zvone.kozull1999@gmail.com>
2025-02-13 18:47:05 +0100

    Prvi commit - dodavanje projekta

---
+++ script.js
@@ -0,0 +1,14 @@
+let name = "Zvone";
+let age = 25;
+let lastName = "Kozul";
+
+console.log(`${name} ${lastName} is ${age} years old.`); // Zvone Kozul is 25 years old.
+console.log(`${name} ${lastName}`); // Zvone Kozul
+
+//podjeli height i weight i ispisi rez
+
+let height = 200;
+let weight = 100;
+let razlika = height / weight;
+console.log(razlika);
+    
